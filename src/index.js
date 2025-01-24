96a4574c8416f7b09d7987daed3f99da9aaae849
Baggiyam Shanmugam <gowribaggiyam@gmail.com>
2025-01-24 14:02:07 +0100

    activity without bonus

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const DURATION = 10; // 10 seconds
+Diff line change
+@@ -2,37 +2,45 @@ const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
 
@@ -11,18 +12,35 @@ let timer = null; // Variable to store the interval
 
 
 
+const startBtn = document.getElementById("start-btn");
+startBtn.addEventListener("click", () => {
+  startCountdown();
+});
 // ITERATION 2: Start Countdown
 function startCountdown() {
   console.log("startCountdown called!");
 
 
   // Your code goes here ...
+  remainingTime = 10;
+  let i = remainingTime;
+  let timeElement = document.getElementById("time");
+  timeElement.innerText = i;
+  const intervalId = setInterval(function () {
+    i--;
+    if (i > 0) {
+      timeElement.innerText = i;
+    } else {
+      clearInterval(intervalId);
+    }
+  }, 1000);
 }
 
 
 
 
 // ITERATION 3: Show Toast
+/* ITERATION 3: Show Toast*/
 function showToast(message) {
   console.log("showToast called!");
 
@@ -31,6 +49,14 @@ function showToast(message) {
 
 
 
+  function showToast() {
+    const toast = document.getElementById("toast");
+    toast.classList.add("show");
+
+    setTimeout(() => {
+      toast.classList.remove("show");
+    }, 3000);
+  }
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
 
   // Your code goes here ...
